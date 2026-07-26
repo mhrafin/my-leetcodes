f85357d86a618094277d8b2741ee288d5f4c2b74
Raf-PC <110781737+mhrafin@users.noreply.github.com>
2026-07-26 17:27:54 +0600

    Switch to markdown files

--- 2634. Filter Elements from Array/submission.tsx
+++
@@ -1,9 +0,0 @@
-type Fn = (n: number, i: number) => any
-
-function filter(arr: number[], fn: Fn): number[] {
-    let final_arr = []
-    for (let i = 0; i < arr.length; i++) {
-        if (fn(arr[i],i)) {final_arr.push(arr[i])}
-    }
-    return final_arr
-};
--- 2635. Apply Transform Over Each Element in Array/submission.tsx
+++
@@ -1,7 +0,0 @@
-function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    let final_arr = []
-    for (let i = 0; i < arr.length; i++) {
-        final_arr.push(fn(arr[i],i))
-    }
-    return final_arr
-};
